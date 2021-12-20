921c8a1984ef3be45fc75206de3b2ebe04b49c72
EunkyuLee <lek7146@gmail.com>
2021-12-21 00:05:16 +0900

    2441 success

--- baekjoon/2441/2441.c
+++ baekjoon/2441/2441.c
@@ -6,7 +6,11 @@ int main(int argc, char const *argv[])
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
     {
-        for (int j = n-i; j > 0; j--)
+        for (int j = 0; j < i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < n-i; k++)
         {
             printf("*");
         }
