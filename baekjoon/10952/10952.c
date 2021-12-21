597392153252f9bee411585acff478aa6a09a5ec
EunkyuLee <lek7146@gmail.com>
2021-12-21 18:33:00 +0900

    10952 success

--- baekjoon/10952/10952.c
+++ baekjoon/10952/10952.c
@@ -2,11 +2,14 @@
 
 int main(int argc, char const *argv[])
 {
-    int a, b, n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    int a, b;
+    while (1)
     {
         scanf("%d %d", &a, &b);
+        if (a == b && a == 0 && b == 0)
+        {
+            break;
+        }
         printf("%d\n", a+b);
     }
     
