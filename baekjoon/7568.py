0ca329703e61fdcc2be6cf9187fb4f98c1b600d2
EunkyuLee <lek7146@gmail.com>
2022-01-01 11:47:14 +0900

    7568 success

---
+++ baekjoon/7568.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(input())
+a = []
+for i in range(n):
+    a.append(list(map(int, sys.stdin.readline().split())))
+
+for i in a:
+    rank = 1
+    for j in a:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=' ')
