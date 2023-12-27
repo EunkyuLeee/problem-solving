4d12226fd70549a31f0f1291793c65419d2121b9
eunkyu <lek7146@gmail.com>
2023-12-27 14:12:14 +0900

    1920 success

--- baekjoon/1920.py
+++ baekjoon/1920.py
@@ -1,11 +1,24 @@
 import sys
 
+
+def binSearch(num, arr, s, e):
+    mid = (s + e) // 2
+    if s == e:
+        return 1 if arr[mid] == num else 0
+    elif num < arr[mid]:
+        return binSearch(num, arr, s, mid)
+    elif num > arr[mid]:
+        return binSearch(num, arr, mid + 1, e)
+    else:
+        return 1
+
+
 n = int(input())
 a = list(map(int, sys.stdin.readline().split()))
 m = int(input())
 b = list(map(int, sys.stdin.readline().split()))
+
+a.sort()
+
 for i in b:
-    if i in a:
-        print(1)
-    else:
-        print(0)
+    print(binSearch(i, a, 0, n - 1))
